f77ebc1330fbbbdf54bff093f9ed46344e9af82d
lzj <lizhijian@jimilab.com>
2020-10-16 10:54:26 +0800

    1、添加ZJModel键值对ZJModelPairs；

--- ZJBaseUtils/ZJModel/ZJModel.m
+++ ZJBaseUtils/ZJModel/ZJModel.m
@@ -9,51 +9,13 @@
 #import "ZJModel.h"
 #import "NSObject+ZJExt.h"
 #import <objc/runtime.h>
-#import "ZJModelFilter.h"
-#import "ZJModelFilterGet.h"
-#import "ZJModelFilterSet.h"
+#import "ZJModelPairs.h"
 
 @implementation ZJModel
 
-- (NSMutableDictionary *)toDictionary
++ (ZJModelType)modelType
 {
-    unsigned int count = 0;
-    objc_property_t *properties = class_copyPropertyList([self class], &count);
-    Ivar *ivar = class_copyIvarList([self class], &count);
-    
-    NSString *filterName = [NSString stringWithFormat:@"@\"%s\"", class_getName([ZJModelFilter class])];
-    NSString *filterGetName = [NSString stringWithFormat:@"@\"%s\"", class_getName([ZJModelFilterGet class])];
-    
-    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    for (int i = 0; i < count; i++) {
-        objc_property_t property = properties[i];
-        NSString *propertyName = [[NSString alloc] initWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
-        NSString *typeName = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivar[i])];
-        
-        id propertyValue = [self valueForKey:propertyName];
-        if (!propertyValue || [typeName isEqualToString:filterName] || [typeName isEqualToString:filterGetName]) {
-            continue;
-        } else if ([propertyValue isKindOfClass:[NSDictionary class]]) {
-            propertyValue = [NSDictionary dictionaryWithDictionary:propertyValue];
-        } else if ([propertyValue isKindOfClass:[NSArray class]]) {
-            propertyValue = [NSArray arrayWithArray:propertyValue];
-        } else if ([[propertyValue class] respondsToSelector:@selector(initWithDic:)]) {
-            propertyValue = [propertyValue toDictionary];
-            if (!propertyValue) {
-                continue;
-            }
-        }
-        
-        [dict setObject:propertyValue forKey:propertyName];
-    }
-    if (properties) {
-        free(properties);
-    }
-    if (ivar) {
-        free(ivar);
-    }
-    
-    return dict;
+    return ZJModelTypeGeneral;
 }
 
 + (instancetype)initWithDic:(NSDictionary *)dic
@@ -63,31 +25,26 @@
     unsigned int count = 0;
     objc_property_t *properties = class_copyPropertyList(self, &count);
     Ivar *ivars = class_copyIvarList(self, nil);
-    
-    NSString *filterName = [NSString stringWithFormat:@"@\"%s\"", class_getName([ZJModelFilter class])];
-    NSString *filterGetName = [NSString stringWithFormat:@"@\"%s\"", class_getName([ZJModelFilterSet class])];
+    if (!properties || !ivars) return model;
     
     for (int i = 0; i < count; i ++) {
         objc_property_t property = properties[i];
         NSString *propertyName = [[NSString alloc] initWithCString:property_getName(property)  encoding:NSUTF8StringEncoding];
+        NSString *typeName = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivars[i])];
         
         id propertyValue = [dic objectForKey:propertyName];
         if (propertyValue) {
-            NSString *typeName = [NSString stringWithCString:ivar_getTypeEncoding(ivars[i]) encoding:NSUTF8StringEncoding];
-            if ([typeName isEqualToString:filterName] || [typeName isEqualToString:filterGetName]) {
-                continue;
-            }
-            
             if (typeName.length > 3) {
                 typeName = [typeName substringWithRange:NSMakeRange(2, typeName.length - 3)];
                 Class cls = objc_getClass([typeName UTF8String]);
-                if ([propertyValue isKindOfClass:[NSDictionary class]]) {
-                    if ([cls respondsToSelector:@selector(initWithDic:)]) {
+                if (cls && [cls respondsToSelector:@selector(modelType)]) {     //自定义Model类型
+                    ZJModelType type = [cls modelType];
+                    if (type == ZJModelTypeDisable || type == ZJModelTypeGet) {
+                        continue;
+                    } else if (type == ZJModelTypePairs) {
+                        propertyValue = [cls initWithName:propertyName withValue:propertyValue];
+                    } else if ([propertyValue isKindOfClass:[NSDictionary class]]) {
                         propertyValue = [cls initWithDic:propertyValue];
-                    }
-                } else {
-                    if ([cls respondsToSelector:@selector(initWithName:value:)]) {
-                        propertyValue = [cls initWithName:propertyName value:propertyValue];
                     }
                 }
             }
@@ -95,21 +52,67 @@
             [model setValue:propertyValue forKey:propertyName];
         }
     }
-    
-    if (properties) {
-        free(properties);
-    }
-    if (ivars) {
-        free(ivars);
-    }
+    free(properties);
+    free(ivars);
     
     return model;
 }
 
-+ (instancetype)initWithName:(NSString *)name value:(NSArray *)value
+
+#pragma mark -
+
+- (NSMutableDictionary *)toDictionary
 {
-    id model = [[self alloc] init];
-    return model;
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    unsigned int count = 0;
+    objc_property_t *properties = class_copyPropertyList([self class], &count);
+    Ivar *ivars = class_copyIvarList([self class], &count);
+    if (!properties || !ivars) return dict;
+    
+    for (int i = 0; i < count; i++) {
+        objc_property_t property = properties[i];
+        NSString *propertyName = [[NSString alloc] initWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
+        NSString *typeName = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivars[i])];
+        
+        id propertyValue = [self valueForKey:propertyName];
+        if (!propertyValue) {
+            continue;
+        }
+        if (typeName.length > 3) {
+            typeName = [typeName substringWithRange:NSMakeRange(2, typeName.length - 3)];
+            Class cls = objc_getClass([typeName UTF8String]);
+            if (cls && [cls respondsToSelector:@selector(modelType)]) {     //自定义Model类型
+                ZJModelType type = [cls modelType];
+                if (type == ZJModelTypeDisable || type == ZJModelTypeSet) {
+                    continue;
+                } else if (type == ZJModelTypePairs) {
+                    propertyName = ((ZJModelPairs *)propertyValue).name;
+                    if (!propertyName) {
+                        continue;
+                    }
+                    propertyValue = ((ZJModelPairs *)propertyValue).value;
+                } else {
+                    propertyValue = [propertyValue toDictionary];
+                    if (!propertyValue) {
+                        continue;
+                    }
+                }
+            }
+        }
+        
+        if ([propertyValue isKindOfClass:[NSDictionary class]]) {
+            propertyValue = [NSDictionary dictionaryWithDictionary:propertyValue];
+        } else if ([propertyValue isKindOfClass:[NSArray class]]) {
+            propertyValue = [NSArray arrayWithArray:propertyValue];
+        }
+        
+        [dict setObject:propertyValue forKey:propertyName];
+    }
+    
+    free(properties);
+    free(ivars);
+    
+    return dict;
 }
 
 @end
---
+++ ZJBaseUtils/ZJModel/ZJModelPairs.m
@@ -0,0 +1,27 @@
+//
+//  ZJModelPairs.m
+//  ZJBaseUtils
+//
+//  Created by 李治健 on 2020/10/16.
+//  Copyright © 2020 ZJ<lizhijian_21@163.com>. All rights reserved.
+//
+
+#import "ZJModelPairs.h"
+
+@implementation ZJModelPairs
+
++ (ZJModelType)modelType
+{
+    return ZJModelTypePairs;
+}
+
++ (instancetype)initWithName:(NSString *)name withValue:(id)value
+{
+    ZJModelPairs *model = [[ZJModelPairs alloc] init];
+    model.name = name;
+    model.value = value;
+    
+    return model;
+}
+
+@end
