77d1e5f8b40b22fd94c1af9c3d928574af83c806
lzj <lizhijian@jimilab.com>
2020-11-03 09:04:27 +0800

    修复本地化语言ZJLocalizationTool；

--- ZJBaseUtils/Device/ZJLocalizationTool.h
+++ ZJBaseUtils/Device/ZJLocalizationTool.h
@@ -11,12 +11,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+extern NSString *const kLocalizationLanguageEn;     //英文
+extern NSString *const kLocalizationLanguageZh_Hans;     //简体中文
+extern NSString *const kLocalizationLanguageRu;     //俄罗斯
+extern NSString *const kLocalizationLanguageFr;     //法文
+extern NSString *const kLocalizationLanguageIt;     //意大利文
+extern NSString *const kLocalizationLanguageEs;     //西班牙文
+
+extern NSString *const kLocalizationLanguageNotiChanged;    //语言变更通知
+
 @interface ZJLocalizationTool : NSObject
 singleton_h();
 
 /// 设置本地语言包
 /// @param languageStr 本地化语言缩写
-- (void)setLocaleLanguage:(NSString *)languageStr;
+- (void)setLanguage:(NSString *)languageStr;
 
 @end
 
--- ZJBaseUtils/Device/ZJLocalizationTool.m
+++ ZJBaseUtils/Device/ZJLocalizationTool.m
@@ -8,7 +8,15 @@
 
 #import "ZJLocalizationTool.h"
 
-#define kZJLocalizationLanguage @"kZJLocalizationLanguage"
+NSString *const kLocalizationLanguageEn = @"en";     //英文
+NSString *const kLocalizationLanguageZh_Hans = @"zh-Hans";     //简体中文
+NSString *const kLocalizationLanguageRu = @"ru";     //俄罗斯
+NSString *const kLocalizationLanguageFr = @"fr";     //法文
+NSString *const kLocalizationLanguageIt = @"it";     //意大利文
+NSString *const kLocalizationLanguageEs = @"es";     //西班牙文
+
+NSString *const kLocalizationLanguage = @"kLocalizationLanguage";
+NSString *const kLocalizationLanguageNotiChanged = "kLocalizationLanguageChanged";    //语言变更通知
 
 @interface ZJLocalizationTool ()
 
@@ -40,14 +48,15 @@ singleton_m();
     [[NSUserDefaults standardUserDefaults] setObject:languageStr forKey:kZJLocalizationLanguage];
     [[NSUserDefaults standardUserDefaults] synchronize];
     self.currentLanguage = [self loadLanguageBundle];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:kLocalizationLanguageNotiChanged object:nil];
 }
 
 - (NSString *)loadLanguageBundle {
-    NSString *defaultLanguage = [[NSUserDefaults standardUserDefaults] objectForKey:kZJLocalizationLanguage];
-    NSString *systemLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
-    if (!defaultLanguage || ![defaultLanguage isEqualToString:systemLanguage]) {
-        defaultLanguage = systemLanguage;
-        [[NSUserDefaults standardUserDefaults] setObject:defaultLanguage forKey:kZJLocalizationLanguage];
+    NSString *defaultLanguage = [[NSUserDefaults standardUserDefaults] objectForKey:kLocalizationLanguage];
+    if (!defaultLanguage) {
+        defaultLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
+        [[NSUserDefaults standardUserDefaults] setObject:defaultLanguage forKey:kLocalizationLanguage];
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
     
