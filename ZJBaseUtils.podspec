ede721cba8c414a52df221b33c2c8e2010ea387f
lzj <lizhijian_21@163.com>
2020-11-18 22:15:07 +0800

    1、添加ZJBaseTabBarConfig可设置不同选择的字体；

--- ZJBaseUtils.podspec
+++ ZJBaseUtils.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "ZJBaseUtils"
-  s.version      = "1.0.beta2"
+  s.version      = "1.0.beta3"
   s.summary      = "Provide common、general、basic API and extensions for iOS Platform."
 
   s.description  = <<-DESC
--- ZJBaseUtils/ZJBaseTabBarController/ZJBaseTabBarButton.m
+++ ZJBaseUtils/ZJBaseTabBarController/ZJBaseTabBarButton.m
@@ -93,7 +93,6 @@
     
     _selected = selected;
     if (selected) {
-        self.titleLB.textColor = self.config.selTitleColor;
         self.imageView.image = [UIImage imageNamed:self.item.selImageName];
         
         if (self.config.animType == ZJBTBConfigAnimTypeRotationY) {
@@ -106,10 +105,12 @@
             [self.imageView.layer addAnimation:[CAAnimation zj_boundsAnimation:CGPointMake(46, 46)] forKey:@"max"];
         }
     } else {
-        self.titleLB.textColor = self.config.norTitleColor;
-        self.imageView.image = [UIImage imageNamed:self.item.norImageName];
         [self.imageView.layer removeAllAnimations];
+        self.imageView.image = [UIImage imageNamed:self.item.norImageName];
     }
+    
+    self.titleLB.textColor = self.selected ? self.config.selTitleColor : self.config.norTitleColor;
+    self.titleLB.font = self.selected ? self.config.selTitleFont : self.config.nolTitleFont;
 }
 
 - (void)setItem:(ZJBaseTarbarItem *)item
